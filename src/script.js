619662667024c6b10cb827e87cb03e5bdc46d254
Wlodzimierz Reif <Wlodzimierz.Reif@Nology.local>
2020-04-09 12:02:18 +0100

    Changed About me section

--- src/script.js
+++ src/script.js
@@ -7,51 +7,40 @@ function myFunction() {
   }
 }
 
-// const activateLink = () => {
-//   const x = document.querySelector('.caption-link');
-//   if (x.classList.contains('disabled')) {
-//     x.classList.remove('disabled');
-//   }
-// }
+const focusAttention = () => {
+  const icons = document.querySelectorAll(".contact-ico");
+  icons.forEach(icon => icon.classList.add("fade"));
+  setTimeout(() => icons.forEach(icon => icon.classList.remove("fade")), 1000);
+  window.scrollTo(0, 0);
+}
 
+const scrollFocus = () => {
+  setTimeout(() => focusAttention(), 1000);
+}
 
+const toggleSection = () => {
+  const x = document.getElementById("additional-info");
+  if (x.classList.contains('slideup')) {
+    x.classList.remove('slideup');
+    x.classList.add("slidedown");
+    setTimeout(() => x.scrollIntoView(), 750);
+  } else {
+    x.classList.remove("slidedown");
+    x.classList.add('slideup');
+  }
+}
 
+// const date1 = new Date();
+// const date2 = new Date('2020/04/17');
+// const diffTime = Math.abs(date2 - date1);
+// const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+// let percent = Math.floor((81 - diffDays) / 81 * 100);
 
-const date1 = new Date();
-const date2 = new Date('2020/04/17');
-const diffTime = Math.abs(date2 - date1);
-const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-// console.log(diffDays);
-let percent = Math.floor((81 - diffDays) / 81 * 100);
-// console.log(percent);
+// const progress = document.querySelector('#about > div > span');
+// progress.style.width = `${percent}%`;
 
-const progress = document.querySelector('#about > div > span');
-progress.style.width = `${percent}%`;
+// const completed = document.querySelector('#completion');
+// completed.textContent = `${percent}% completed`;
 
-const completed = document.querySelector('#completion');
-completed.textContent = `${percent}% completed`;
-
-const incompleted = document.querySelector('#incompletion');
-incompleted.textContent = `${100 - percent}% to go`;
-
-
-
-
-
-
-
-// function showStuff() {
-//   var history = setInterval(function () { showHistory() }, 500);
-//   var history = setInterval(function () { showPhotography() }, 1000);
-//   var history = setInterval(function () { showParenting() }, 1500);
-// }
-
-// function showHistory() {
-//   document.querySelector('.history').style.display = 'block';
-// }
-// function showPhotography() {
-//   document.querySelector('.photography').style.display = 'block';
-// }
-// function showParenting() {
-//   document.querySelector('.parenting').style.display = 'block';
-// }
+// const incompleted = document.querySelector('#incompletion');
+// incompleted.textContent = `${100 - percent}% to go`;
