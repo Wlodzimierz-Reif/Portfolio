14e480d893e333599699d20e1db99ed0f0abafad
Wlodzimierz-Reif <wlodzimierzreif@gmail.com>
2021-07-24 20:39:26 +0100

    changed name to Wlodzimierz

--- src/script.js
+++ src/script.js
@@ -1,25 +1,25 @@
-document.addEventListener("click", (event) => {
-  const links = document.getElementById("links");
-  const icon = document.getElementById("icon");
-  if (!links.classList.contains("hidden") && !icon.contains(event.target)) {
-    links.classList.add("hidden");
+document.addEventListener('click', (event) => {
+  const links = document.getElementById('links');
+  const icon = document.getElementById('icon');
+  if (!links.classList.contains('hidden') && !icon.contains(event.target)) {
+    links.classList.add('hidden');
   }
 });
 
 const toggleMenu = () => {
-  const x = document.getElementById("links");
-  if (x.classList.contains("hidden")) {
-    x.classList.remove("hidden");
+  const x = document.getElementById('links');
+  if (x.classList.contains('hidden')) {
+    x.classList.remove('hidden');
   } else {
-    x.classList.add("hidden");
+    x.classList.add('hidden');
   }
 };
 
 const focusAttention = () => {
-  const icons = document.querySelectorAll(".contact-ico");
-  icons.forEach((icon) => icon.classList.add("fade"));
+  const icons = document.querySelectorAll('.contact-ico');
+  icons.forEach((icon) => icon.classList.add('fade'));
   setTimeout(
-    () => icons.forEach((icon) => icon.classList.remove("fade")),
+    () => icons.forEach((icon) => icon.classList.remove('fade')),
     1000
   );
   window.scrollTo(0, 0);
@@ -30,13 +30,13 @@ const scrollFocus = () => {
 };
 
 const toggleSection = () => {
-  const x = document.getElementById("additional-info");
-  if (x.classList.contains("slideup")) {
-    x.classList.remove("slideup");
-    x.classList.add("slidedown");
+  const x = document.getElementById('additional-info');
+  if (x.classList.contains('slideup')) {
+    x.classList.remove('slideup');
+    x.classList.add('slidedown');
     setTimeout(() => x.scrollIntoView(), 500);
   } else {
-    x.classList.remove("slidedown");
-    x.classList.add("slideup");
+    x.classList.remove('slidedown');
+    x.classList.add('slideup');
   }
 };
