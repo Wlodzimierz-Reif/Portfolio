01bdd9ca04b9d711b3811f04936f582fdfc3a547
Wlodzimierz-Reif <wlodzimierzreif@gmail.com>
2021-07-28 11:27:00 +0100

    added typewriter animation, fixed mobile view

--- src/script.js
+++ src/script.js
@@ -30,16 +30,6 @@ const scrollFocus = () => {
   setTimeout(() => focusAttention(), window.scrollY / 1.5); // detects how far window is scrolled down and changes delay
 };
 
-// couldn't add event listener the usuall way as HTML was not build by that time(not every time, just most of the time!!!)
-window.onload = () => {
-  const contact = document.querySelector('#contact-button');
-  const burgerMenu = document.querySelector('#icon');
-  // const moreInfo = document.querySelector('#more-info');
-  contact.addEventListener('click', scrollFocus);
-  burgerMenu.addEventListener('click', toggleMenu);
-  // moreInfo.addEventListener('click', toggleSection);
-};
-
 const projects = document.querySelectorAll('.project');
 
 projects.forEach((element) => {
@@ -47,6 +37,7 @@ projects.forEach((element) => {
   console.log('changed opacity to 0');
 });
 
+// checks if card .project is in view
 const projectInView = (project, percentageScroll = 100) => {
   const projectTop = project.getBoundingClientRect().top;
   const projectBottom = project.getBoundingClientRect().bottom;
@@ -77,6 +68,26 @@ const handleScrollAnimation = () => {
       if (project.classList.contains('scrolled')) hideScrollElement(project);
     }
   });
+};
+
+let i = 0;
+const txt = 'Junior Web Developer';
+
+const typeWriter = () => {
+  if (i < txt.length) {
+    document.querySelector('#subtitle').innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, 100);
+  }
+};
+
+// couldn't add event listener the usuall way as HTML was not build by that time(not every time, just most of the time!!!)
+window.onload = () => {
+  const contact = document.querySelector('#contact-button');
+  const burgerMenu = document.querySelector('#icon');
+  contact.addEventListener('click', scrollFocus);
+  burgerMenu.addEventListener('click', toggleMenu);
+  typeWriter();
 };
 
 window.addEventListener('scroll', () => {
